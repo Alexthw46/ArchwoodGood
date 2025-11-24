e3ba3d78c445ffe9f3d4b27ff80544e9a1577416
Alexthw <77152778+Alexthw46@users.noreply.github.com>
2025-11-24 19:38:49 +0100

    Remove example stuff, only compat code

---
+++ src/main/java/com/example/an_addon/ArchwoodGood.java
@@ -0,0 +1,43 @@
+package com.example.an_addon;
+
+import com.hollingsworth.arsnouveau.ArsNouveau;
+import net.mehvahdjukaar.every_compat.api.SimpleModule;
+import net.mehvahdjukaar.moonlight.api.set.leaves.LeavesTypeRegistry;
+import net.mehvahdjukaar.moonlight.api.set.wood.WoodTypeRegistry;
+
+public class ArchwoodGood extends SimpleModule {
+
+    public ArchwoodGood(String modid) {
+        super(modid, "awg", ArsNouveau.MODID);
+
+        WoodTypeRegistry woodReg = WoodTypeRegistry.INSTANCE;
+        woodReg.addSimpleFinder("ars_nouveau", "purple_archwood").planks("archwood_planks");
+
+        woodReg.addSimpleFinder("ars_nouveau", "blue_archwood")
+                .planks("archwood_planks");
+
+        woodReg.addSimpleFinder("ars_nouveau", "green_archwood")
+                .planks("archwood_planks");
+
+        woodReg.addSimpleFinder("ars_nouveau", "red_archwood")
+                .planks("archwood_planks");
+
+        woodReg.addSimpleFinder("ars_elemental", "yellow_archwood")
+                .planks("archwood_planks");
+
+        LeavesTypeRegistry leafReg = LeavesTypeRegistry.INSTANCE;
+
+        leafReg.addSimpleFinder("ars_nouveau", "purple_archwood");
+        leafReg.addSimpleFinder("ars_nouveau", "blue_archwood");
+        leafReg.addSimpleFinder("ars_nouveau", "green_archwood");
+        leafReg.addSimpleFinder("ars_nouveau", "red_archwood");
+        leafReg.addSimpleFinder("ars_elemental", "yellow_archwood");
+
+        leafReg.addLeavesToWoodMapping("ars_nouveau:purple_archwood", "ars_nouveau:purple_archwood");
+        leafReg.addLeavesToWoodMapping("ars_nouveau:blue_archwood", "ars_nouveau:blue_archwood");
+        leafReg.addLeavesToWoodMapping("ars_nouveau:green_archwood", "ars_nouveau:green_archwood");
+        leafReg.addLeavesToWoodMapping("ars_nouveau:red_archwood", "ars_nouveau:red_archwood");
+        leafReg.addLeavesToWoodMapping("ars_elemental:yellow_archwood", "ars_elemental:yellow_archwood");
+
+    }
+}
