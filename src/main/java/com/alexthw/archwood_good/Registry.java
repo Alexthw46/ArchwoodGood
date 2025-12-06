406305e3615c51e6cbc97adcef2adacf1fd9abd3
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2025-12-05 20:01:33 -0700

    Registry & ElementalModule: Added WOOD_PROP & SAP_PROP with "final"

--- src/main/java/com/alexthw/archwood_good/Registry.java
+++ src/main/java/com/alexthw/archwood_good/Registry.java
@@ -1,46 +1,51 @@
 package com.alexthw.archwood_good;
 
-import com.alexthw.archwood_good.integration.ElementalModule;
+import com.alexthw.archwood_good.common.block.StrippablePlanks;
 import com.alexthw.archwood_good.integration.CompatRegistry;
+import com.alexthw.archwood_good.integration.ElementalModule;
 import com.hollingsworth.arsnouveau.common.block.MagicLeaves;
 import com.hollingsworth.arsnouveau.common.block.StrippableLog;
 import com.hollingsworth.arsnouveau.common.world.tree.MagicTree;
-import com.hollingsworth.arsnouveau.setup.registry.BlockRegistry;
 import com.hollingsworth.arsnouveau.setup.registry.BlockRegistryWrapper;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.level.material.PushReaction;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModList;
 
 import static com.hollingsworth.arsnouveau.setup.registry.BlockRegistry.*;
 
 public class Registry {
+
+    public static final BlockBehaviour.Properties WOOD_PROP = BlockBehaviour.Properties.of().mapColor(MapColor.WOOD).strength(2.0F, 3.0F).ignitedByLava().sound(SoundType.WOOD);
+    public static final BlockBehaviour.Properties SAP_PROP = BlockBehaviour.Properties.of().noCollission().randomTicks().instabreak().sound(SoundType.GRASS).pushReaction(PushReaction.DESTROY);
+
     /// ─────────────────────────────────── Archwood ────────────────────────────────────
-    public static BlockRegistryWrapper<RotatedPillarBlock> STRIPPED_ARCHWOOD_LOG = registerBlockAndItem("stripped_archwood_log", () ->
-            new RotatedPillarBlock(LOG_PROP));
+    public static BlockRegistryWrapper<RotatedPillarBlock> STRIPPED_FADING_ARCHWOOD_LOG = registerBlockAndItem("stripped_archwood_log", () ->
+            new RotatedPillarBlock(WOOD_PROP));
 
-    public static BlockRegistryWrapper<StrippableLog> ARCHWOOD_LOG = registerBlockAndItem("archwood_log", () ->
-            new StrippableLog(LOG_PROP, () -> STRIPPED_ARCHWOOD_LOG.get()));
+    public static BlockRegistryWrapper<StrippableLog> FADING_ARCHWOOD_LOG = registerBlockAndItem("archwood_log", () ->
+            new StrippableLog(WOOD_PROP, () -> STRIPPED_FADING_ARCHWOOD_LOG.get()));
 
-    public static BlockRegistryWrapper<SaplingBlock> ARCHWOOD_SAPLING = registerBlockAndItem("archwood_sapling", () ->
-            new SaplingBlock(MagicTree.getGrower("archwood_tree", AWGWorldgenRegistry.CONFIGURED_ARCHWOOD_TREE), SAP_PROP));
+    public static BlockRegistryWrapper<SaplingBlock> FADING_ARCHWOOD_SAPLING = registerBlockAndItem("archwood_sapling", () ->
+            new SaplingBlock(MagicTree.getGrower("archwood_tree", AWGWorldgenRegistry.CONFIGURED_FADING_TREE), SAP_PROP));
 
-    public static BlockRegistryWrapper<MagicLeaves> ARCHWOOD_LEAVES = registerBlockAndItem("archwood_leaves", () ->
+    public static BlockRegistryWrapper<MagicLeaves> FADING_ARCHWOOD_LEAVES = registerBlockAndItem("archwood_leaves", () ->
             createLeavesBlock(MapColor.COLOR_GRAY));
 
     /// ──────────────────────────────────── Planks ─────────────────────────────────────
     public static BlockRegistryWrapper<Block> BLUE_ARCHWOOD_PLANK = registerBlockAndItem("blue_archwood_planks", () ->
-            new Block(BlockBehaviour.Properties.ofFullCopy(BlockRegistry.ARCHWOOD_PLANK.get())));
+            new StrippablePlanks(WOOD_PROP, ARCHWOOD_PLANK));
 
     public static BlockRegistryWrapper<Block> RED_ARCHWOOD_PLANK = registerBlockAndItem("red_archwood_planks", () ->
-            new Block(BlockBehaviour.Properties.ofFullCopy(BlockRegistry.ARCHWOOD_PLANK.get())));
+            new StrippablePlanks(WOOD_PROP, ARCHWOOD_PLANK));
 
     public static BlockRegistryWrapper<Block> GREEN_ARCHWOOD_PLANK = registerBlockAndItem("green_archwood_planks", () ->
-            new Block(BlockBehaviour.Properties.ofFullCopy(BlockRegistry.ARCHWOOD_PLANK.get())));
+            new StrippablePlanks(WOOD_PROP, ARCHWOOD_PLANK));
 
     public static BlockRegistryWrapper<Block> PURPLE_ARCHWOOD_PLANK = registerBlockAndItem("purple_archwood_planks", () ->
-            new Block(BlockBehaviour.Properties.ofFullCopy(BlockRegistry.ARCHWOOD_PLANK.get())));
+            new StrippablePlanks(WOOD_PROP, ARCHWOOD_PLANK));
 
     /// ───────────────────────────────────── Slab ──────────────────────────────────────
     public static BlockRegistryWrapper<Block> BLUE_ARCHWOOD_SLAB = registerBlockAndItem("blue_archwood_slab", () ->
@@ -73,6 +78,7 @@ public class Registry {
         if (ModList.get().isLoaded("ars_elemental")) {
             ElementalModule.init();
         }
+        // if Every Compat is loaded, register the module
         if (ModList.get().isLoaded("everycomp")) {
             CompatRegistry.init();
         }
--- src/main/java/com/alexthw/archwood_good/integration/ElementalModule.java
+++ src/main/java/com/alexthw/archwood_good/integration/ElementalModule.java
@@ -1,16 +1,24 @@
 package com.alexthw.archwood_good.integration;
 
 import alexthw.ars_elemental.registry.ModItems;
+import com.alexthw.archwood_good.common.block.StrippablePlanks;
+import com.hollingsworth.arsnouveau.common.block.ModBlock;
 import com.hollingsworth.arsnouveau.setup.registry.BlockRegistry;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SlabBlock;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.StairBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
 import net.neoforged.neoforge.registries.DeferredHolder;
 
 import java.util.function.Supplier;
+
+import static com.alexthw.archwood_good.Registry.WOOD_PROP;
+import static com.hollingsworth.arsnouveau.setup.registry.BlockRegistry.ARCHWOOD_PLANK;
+import static com.hollingsworth.arsnouveau.setup.registry.BlockRegistry.LOG_PROP;
 
 public class ElementalModule {
 
@@ -18,7 +26,7 @@ public class ElementalModule {
     }
 
     public static DeferredHolder<Block, ? extends Block> YELLOW_ARCHWOOD_PLANK = addBlock("yellow_archwood_planks", () ->
-            new Block(BlockBehaviour.Properties.ofFullCopy(BlockRegistry.ARCHWOOD_PLANK.get())));
+            new StrippablePlanks(WOOD_PROP, ARCHWOOD_PLANK));
 
     public static DeferredHolder<Block, ? extends Block> YELLOW_ARCHWOOD_SLAB = addBlock("yellow_archwood_slab", () ->
             new SlabBlock(BlockBehaviour.Properties.ofFullCopy(YELLOW_ARCHWOOD_PLANK.get())));
@@ -26,7 +34,7 @@ public class ElementalModule {
     public static DeferredHolder<Block, ? extends Block> YELLOW_ARCHWOOD_STAIRS = addBlock("yellow_archwood_stairs", () ->
             new StairBlock(YELLOW_ARCHWOOD_PLANK.get().defaultBlockState(), BlockBehaviour.Properties.ofFullCopy(YELLOW_ARCHWOOD_PLANK.get())));
 
-    static DeferredHolder<Block, ? extends Block> addBlock(String name, Supplier<Block> blockSupp) {
+    private static DeferredHolder<Block, ? extends Block> addBlock(String name, Supplier<Block> blockSupp) {
         DeferredHolder<Block, ? extends Block> block = ModItems.BLOCKS.register(name, blockSupp);
         ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
         return block;
